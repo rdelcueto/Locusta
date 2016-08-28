4f0e0319c183fc463ba88254f4b964cd9dfd3051
Rodrigo Gonzalez del Cueto <seltix@localhost.localdomain>
2016-08-28 16:44:10 -0500

    CUDA Genetic Algorithm Unison Data Transform

--- src/solvers/ga/ga_operators/kernel/ga_std_operators_cuda.cu
+++ src/solvers/ga/ga_operators/kernel/ga_std_operators_cuda.cu
@@ -1,264 +1,247 @@
-#include "cuda_common/cuda_helpers.h"
 #include "../ga_std_operators_cuda_impl.hpp"
+#include "cuda_common/cuda_helpers.h"
 
 namespace locusta {
 
-  /// GPU Kernels Shared Memory Pointer.
-  extern __shared__ int ga_operators_shared_memory[];
+/// GPU Kernels Shared Memory Pointer.
+extern __shared__ int ga_operators_shared_memory[];
 
-  template <typename TFloat>
-  __global__
-  void whole_crossover_kernel(const uint32_t DIMENSIONS,
-                              const TFloat DEVIATION,
-                              const TFloat CROSSOVER_RATE,
-                              const TFloat MUTATION_RATE,
-                              const uint32_t DIST_LIMIT,
-                              const TFloat INV_DIST_LIMIT,
-                              const TFloat * __restrict__ VAR_RANGES,
-                              const TFloat * __restrict__ prn_array,
-                              const uint32_t * __restrict__ couple_selection,
-                              const TFloat * __restrict__ parent_genomes,
-                              TFloat * __restrict__ offspring_genomes,
-                              curandState * __restrict__ local_generator) {
+template <typename TFloat>
+__device__ __forceinline__ TFloat
+GeneCrossOver(const TFloat parentA, const TFloat parentB)
+{
+  TFloat offspring_gene = parentA;
+  offspring_gene *= 0.5;
+  offspring_gene += parentB * 0.5;
+  return offspring_gene;
+}
 
-    const uint32_t i = blockIdx.x;
-    const uint32_t j = threadIdx.x;
+template <typename TFloat>
+__device__ __forceinline__ TFloat
+GeneCrop(const TFloat offspring_gene, const TFloat lower_bound,
+         const TFloat upper_bound)
+{
+  TFloat mutated_gene = offspring_gene;
+  mutated_gene = offspring_gene < lower_bound ? lower_bound : offspring_gene;
+  mutated_gene = offspring_gene > upper_bound ? upper_bound : offspring_gene;
+  return mutated_gene;
+}
 
-    const uint32_t ISLES = gridDim.x;
-    const uint32_t AGENTS = blockDim.x;
+template <typename TFloat>
+__global__ void
+whole_crossover_kernel(const uint32_t DIMENSIONS, const TFloat DEVIATION,
+                       const TFloat CROSSOVER_RATE, const TFloat MUTATION_RATE,
+                       const uint32_t DIST_LIMIT, const TFloat INV_DIST_LIMIT,
+                       const TFloat* __restrict__ VAR_RANGES,
+                       const TFloat* __restrict__ LOWER_BOUNDS,
+                       const TFloat* __restrict__ UPPER_BOUNDS,
+                       const TFloat* __restrict__ prn_array,
+                       const uint32_t* __restrict__ couple_selection,
+                       const TFloat* __restrict__ parent_genomes,
+                       TFloat* __restrict__ offspring_genomes,
+                       curandState* __restrict__ local_generator)
+{
 
-    const uint32_t THREAD_OFFSET = ISLES * AGENTS;
-    const uint32_t BASE_IDX = i * AGENTS + j;
+  const uint32_t i = blockIdx.x;
+  const uint32_t j = threadIdx.x;
 
-    curandState local_state = local_generator[BASE_IDX];
+  const uint32_t ISLES = gridDim.x;
+  const uint32_t AGENTS = blockDim.x;
 
-    const TFloat * agent_prns = prn_array + BASE_IDX;
+  const uint32_t THREAD_OFFSET = ISLES * AGENTS;
+  const uint32_t BASE_IDX = i * AGENTS + j;
 
-    TFloat * offspring = offspring_genomes + BASE_IDX;
-    const TFloat * parentA = parent_genomes + BASE_IDX;
+  curandState local_state = local_generator[BASE_IDX];
 
-    const bool CROSSOVER_FLAG = (*agent_prns) < CROSSOVER_RATE;
-    agent_prns += THREAD_OFFSET; // Advance pointer
+  const TFloat* agent_prns = prn_array + BASE_IDX;
 
-    for(uint32_t k = 0; k < DIMENSIONS; ++k) {
+  TFloat* offspring = offspring_genomes + BASE_IDX;
+  const uint32_t couple_idx = couple_selection[BASE_IDX];
+  const TFloat* parentA = parent_genomes + BASE_IDX;
+  const TFloat* parentB = parent_genomes + couple_idx + i * AGENTS;
+
+  const bool CROSSOVER_FLAG = (*agent_prns) < CROSSOVER_RATE;
+  agent_prns += THREAD_OFFSET; // Advance pointer
+
+  // Cross over
+  if (CROSSOVER_FLAG) {
+    for (uint32_t k = 0; k < DIMENSIONS; ++k) {
       offspring[k * THREAD_OFFSET] = parentA[k * THREAD_OFFSET];
     }
-
-    if (CROSSOVER_FLAG) {
-      const uint32_t couple_idx = couple_selection[BASE_IDX];
-      const TFloat * parentB = parent_genomes + couple_idx + i * AGENTS;
-
-      for(uint32_t k = 0; k < DIMENSIONS; ++k) {
-        offspring[k * THREAD_OFFSET] *= 0.5;
-        offspring[k * THREAD_OFFSET] += parentB[k * THREAD_OFFSET] * 0.5;
-      }
+  } else {
+    for (uint32_t k = 0; k < DIMENSIONS; ++k) {
+      offspring[k * THREAD_OFFSET] =
+        GeneCrossOver(parentA[k * THREAD_OFFSET], parentB[k * THREAD_OFFSET]);
     }
+  }
 
-    for(uint32_t k = 0; k < DIMENSIONS; ++k) {
-      const bool GENE_MUTATE_FLAG = (*agent_prns) < MUTATION_RATE;
+  for (uint32_t k = 0; k < DIMENSIONS; ++k) {
+    const TFloat& range = VAR_RANGES[k];
+    const TFloat& lower_bound = LOWER_BOUNDS[k];
+    const TFloat& upper_bound = UPPER_BOUNDS[k];
+    const bool GENE_MUTATE_FLAG = (*agent_prns) < MUTATION_RATE;
+    agent_prns += THREAD_OFFSET; // Advance pointer
+
+    // Mutate Gene
+    if (GENE_MUTATE_FLAG) {
+      TFloat mutated_gene = offspring[k * THREAD_OFFSET];
+      TFloat x = 0.0;
+      for (uint32_t n = 0; n < DIST_LIMIT; ++n) {
+        x += curand_uniform(&local_state);
+      }
+
+      x *= INV_DIST_LIMIT;
+      x -= 0.5;
+      x *= DEVIATION * range;
+
+      mutated_gene += x;
+
+      // Crop gene
+      offspring[k * THREAD_OFFSET] =
+        GeneCrop(mutated_gene, lower_bound, upper_bound);
+    }
+  }
+
+  local_generator[BASE_IDX] = local_state;
+}
+
+template <typename TFloat>
+void
+whole_crossover_dispatch(const uint32_t ISLES, const uint32_t AGENTS,
+                         const uint32_t DIMENSIONS, const TFloat DEVIATION,
+                         const TFloat CROSSOVER_RATE,
+                         const TFloat MUTATION_RATE, const uint32_t DIST_LIMIT,
+                         const TFloat* VAR_RANGES, const TFloat* LOWER_BOUNDS,
+                         const TFloat* UPPER_BOUNDS, const TFloat* prn_array,
+                         const uint32_t* couple_selection,
+                         const TFloat* parent_genomes,
+                         TFloat* offspring_genomes,
+                         prngenerator_cuda<TFloat>* local_generator)
+{
+
+  curandState* device_generators =
+    local_generator->get_device_generator_states();
+  const TFloat INV_DIST_LIMIT = 1.0 / DIST_LIMIT;
+
+  whole_crossover_kernel<<<ISLES, AGENTS>>>(
+    DIMENSIONS, DEVIATION, CROSSOVER_RATE, MUTATION_RATE, DIST_LIMIT,
+    INV_DIST_LIMIT, VAR_RANGES, LOWER_BOUNDS, UPPER_BOUNDS, prn_array,
+    couple_selection, parent_genomes, offspring_genomes, device_generators);
+
+  CudaCheckError();
+}
+
+template void whole_crossover_dispatch<float>(
+  const uint32_t ISLES, const uint32_t AGENTS, const uint32_t DIMENSIONS,
+  const float DEVIATION, const float CROSSOVER_RATE, const float MUTATION_RATE,
+  const uint32_t DIST_LIMIT, const float* VAR_RANGES, const float* LOWER_BOUNDS,
+  const float* UPPER_BOUNDS, const float* prn_array,
+  const uint32_t* couple_selection, const float* parent_genomes,
+  float* offspring_genomes, prngenerator_cuda<float>* local_generator);
+
+template void whole_crossover_dispatch<double>(
+  const uint32_t ISLES, const uint32_t AGENTS, const uint32_t DIMENSIONS,
+  const double DEVIATION, const double CROSSOVER_RATE,
+  const double MUTATION_RATE, const uint32_t DIST_LIMIT,
+  const double* VAR_RANGES, const double* LOWER_BOUNDS,
+  const double* UPPER_BOUNDS, const double* prn_array,
+  const uint32_t* couple_selection, const double* parent_genomes,
+  double* offspring_genomes, prngenerator_cuda<double>* local_generator);
+
+template <typename TFloat>
+__global__ void
+tournament_selection_kernel(const uint32_t SELECTION_SIZE,
+                            const TFloat SELECTION_P,
+                            const TFloat* __restrict__ fitness_array,
+                            const TFloat* __restrict__ prn_array,
+                            uint32_t* __restrict__ couple_idx_array,
+                            uint32_t* __restrict__ candidates_reservoir_array)
+{
+
+  const uint32_t i = blockIdx.x;  // ISLE
+  const uint32_t j = threadIdx.x; // AGENT
+
+  const uint32_t ISLES = gridDim.x;
+  const uint32_t AGENTS = blockDim.x;
+
+  const uint32_t THREAD_OFFSET = ISLES * AGENTS;
+  const uint32_t BASE_IDX = i * AGENTS + j;
+
+  const TFloat* agent_prns = prn_array + BASE_IDX;
+
+  uint32_t* local_candidates = candidates_reservoir_array + BASE_IDX;
+
+  // Resevoir Sampling
+  for (uint32_t k = 0; k < (AGENTS - 1); ++k) {
+    if (k < SELECTION_SIZE) {
+      // Fill
+      local_candidates[k * THREAD_OFFSET] = k < j ? k : k + 1;
+    } else {
+      uint32_t r;
+      r = (*agent_prns) * (k + 1);
       agent_prns += THREAD_OFFSET; // Advance pointer
-
-      if(GENE_MUTATE_FLAG) {
-        // TODO: MOVE TO SHARED MEM -> BROADCAST
-        const TFloat & range = VAR_RANGES[k];
-
-        TFloat x = 0.0;
-        for(uint32_t n = 0; n < DIST_LIMIT; ++n) {
-          x += curand_uniform(&local_state);
-        }
-
-        x *= INV_DIST_LIMIT;
-        x -= 0.5;
-        x *= DEVIATION * range;
-
-        offspring[k * THREAD_OFFSET] += x;
+      if (r < SELECTION_SIZE) {
+        // Replace
+        local_candidates[r * THREAD_OFFSET] = k < j ? k : k + 1;
       }
     }
-
-    local_generator[BASE_IDX] = local_state;
   }
 
-  template <typename TFloat>
-  void whole_crossover_dispatch
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t DIMENSIONS,
-   const TFloat DEVIATION,
-   const TFloat CROSSOVER_RATE,
-   const TFloat MUTATION_RATE,
-   const uint32_t DIST_LIMIT,
-   const TFloat * VAR_RANGES,
-   const TFloat * prn_array,
-   const uint32_t * couple_selection,
-   const TFloat * parent_genomes,
-   TFloat * offspring_genomes,
-   prngenerator_cuda<TFloat> * local_generator) {
+  // Tournament
+  bool switch_flag;
 
-    curandState * device_generators = local_generator->get_device_generator_states();
-    const TFloat INV_DIST_LIMIT = 1.0 / DIST_LIMIT;
+  uint32_t best_idx = *(local_candidates);
+  TFloat best_fitness = fitness_array[best_idx + i * AGENTS];
 
-    whole_crossover_kernel
-      <<<ISLES, AGENTS>>>
-      (DIMENSIONS,
-       DEVIATION,
-       CROSSOVER_RATE,
-       MUTATION_RATE,
-       DIST_LIMIT,
-       INV_DIST_LIMIT,
-       VAR_RANGES,
-       prn_array,
-       couple_selection,
-       parent_genomes,
-       offspring_genomes,
-       device_generators);
+  // TODO: Check prng cardinality.
+  // SELECTION_SIZE - 1
 
-    CudaCheckError();
-  }
+  for (uint32_t k = 1; k < SELECTION_SIZE; ++k) {
+    const uint32_t candidate_idx = local_candidates[k * THREAD_OFFSET];
+    const TFloat candidate_fitness = fitness_array[candidate_idx + i * AGENTS];
 
-  template
-  void whole_crossover_dispatch<float>
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t DIMENSIONS,
-   const float DEVIATION,
-   const float CROSSOVER_RATE,
-   const float MUTATION_RATE,
-   const uint32_t DIST_LIMIT,
-   const float * VAR_RANGES,
-   const float * prn_array,
-   const uint32_t * couple_selection,
-   const float * parent_genomes,
-   float * offspring_genomes,
-   prngenerator_cuda<float> * local_generator);
+    switch_flag = (candidate_fitness > best_fitness);
 
-  template
-  void whole_crossover_dispatch<double>
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t DIMENSIONS,
-   const double DEVIATION,
-   const double CROSSOVER_RATE,
-   const double MUTATION_RATE,
-   const uint32_t DIST_LIMIT,
-   const double * VAR_RANGES,
-   const double * prn_array,
-   const uint32_t * couple_selection,
-   const double * parent_genomes,
-   double * offspring_genomes,
-   prngenerator_cuda<double> * local_generator);
-
-  template <typename TFloat>
-  __global__
-  void tournament_selection_kernel(const uint32_t SELECTION_SIZE,
-                                   const TFloat SELECTION_P,
-                                   const TFloat * __restrict__ fitness_array,
-                                   const TFloat * __restrict__ prn_array,
-                                   uint32_t * __restrict__ couple_idx_array,
-                                   uint32_t * __restrict__ candidates_reservoir_array) {
-
-    const uint32_t i = blockIdx.x; // ISLE
-    const uint32_t j = threadIdx.x; // AGENT
-
-    const uint32_t ISLES = gridDim.x;
-    const uint32_t AGENTS = blockDim.x;
-
-    const uint32_t THREAD_OFFSET = ISLES * AGENTS;
-    const uint32_t BASE_IDX = i * AGENTS + j;
-
-    const TFloat * agent_prns = prn_array + BASE_IDX;
-
-    uint32_t * local_candidates = candidates_reservoir_array + BASE_IDX;
-
-    // Resevoir Sampling
-    for(uint32_t k = 0; k < (AGENTS - 1); ++k) {
-      if (k < SELECTION_SIZE) {
-        // Fill
-        local_candidates[k * THREAD_OFFSET] = k < j ? k : k + 1;
-      } else {
-        uint32_t r;
-        r = (*agent_prns) * (k + 1);
-        agent_prns += THREAD_OFFSET; // Advance pointer
-        if (r < SELECTION_SIZE) {
-          // Replace
-          local_candidates[r * THREAD_OFFSET] = k < j ? k : k + 1;
-        }
-      }
+    if ((SELECTION_P != 0.0f) && (SELECTION_P >= (*agent_prns))) {
+      switch_flag = !switch_flag;
     }
 
-    // Tournament
-    bool switch_flag;
+    agent_prns += THREAD_OFFSET; // Advance pointer
 
-    uint32_t best_idx = *(local_candidates);
-    TFloat best_fitness = fitness_array[best_idx + i * AGENTS];
-
-    // TODO: Check prng cardinality.
-    // SELECTION_SIZE - 1
-
-    for(uint32_t k = 1; k < SELECTION_SIZE; ++k) {
-      const uint32_t candidate_idx = local_candidates[k * THREAD_OFFSET];
-      const TFloat candidate_fitness = fitness_array[candidate_idx + i * AGENTS];
-
-      switch_flag = (candidate_fitness > best_fitness);
-
-      if((SELECTION_P != 0.0f) &&
-         (SELECTION_P >= (*agent_prns))) {
-        switch_flag = !switch_flag;
-      }
-
-      agent_prns += THREAD_OFFSET; // Advance pointer
-
-      if(switch_flag) {
-        best_fitness = candidate_fitness;
-        best_idx = candidate_idx;
-      }
+    if (switch_flag) {
+      best_fitness = candidate_fitness;
+      best_idx = candidate_idx;
     }
-
-    couple_idx_array[BASE_IDX] = best_idx;
   }
 
+  couple_idx_array[BASE_IDX] = best_idx;
+}
 
-  template <typename TFloat>
-  void tournament_selection_dispatch
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t SELECTION_SIZE,
-   const TFloat SELECTION_P,
-   const TFloat * fitness_array,
-   const TFloat * prn_array,
-   uint32_t * couple_idx_array,
-   uint32_t * candidates_reservoir_array) {
-    tournament_selection_kernel
-      <<<ISLES, AGENTS>>>
-      (SELECTION_SIZE,
-       SELECTION_P,
-       fitness_array,
-       prn_array,
-       couple_idx_array,
-       candidates_reservoir_array);
+template <typename TFloat>
+void
+tournament_selection_dispatch(const uint32_t ISLES, const uint32_t AGENTS,
+                              const uint32_t SELECTION_SIZE,
+                              const TFloat SELECTION_P,
+                              const TFloat* fitness_array,
+                              const TFloat* prn_array,
+                              uint32_t* couple_idx_array,
+                              uint32_t* candidates_reservoir_array)
+{
+  tournament_selection_kernel<<<ISLES, AGENTS>>>(
+    SELECTION_SIZE, SELECTION_P, fitness_array, prn_array, couple_idx_array,
+    candidates_reservoir_array);
 
-    CudaCheckError();
-  }
+  CudaCheckError();
+}
 
-  template
-  void tournament_selection_dispatch<float>
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t SELECTION_SIZE,
-   const float SELECTION_P,
-   const float * fitness_array,
-   const float * prn_array,
-   uint32_t * couple_idx_array,
-   uint32_t * candidates_reservoir_array);
+template void tournament_selection_dispatch<float>(
+  const uint32_t ISLES, const uint32_t AGENTS, const uint32_t SELECTION_SIZE,
+  const float SELECTION_P, const float* fitness_array, const float* prn_array,
+  uint32_t* couple_idx_array, uint32_t* candidates_reservoir_array);
 
-  template
-  void tournament_selection_dispatch<double>
-  (const uint32_t ISLES,
-   const uint32_t AGENTS,
-   const uint32_t SELECTION_SIZE,
-   const double SELECTION_P,
-   const double * fitness_array,
-   const double * prn_array,
-   uint32_t * couple_idx_array,
-   uint32_t * candidates_reservoir_array);
-
+template void tournament_selection_dispatch<double>(
+  const uint32_t ISLES, const uint32_t AGENTS, const uint32_t SELECTION_SIZE,
+  const double SELECTION_P, const double* fitness_array,
+  const double* prn_array, uint32_t* couple_idx_array,
+  uint32_t* candidates_reservoir_array);
 }
--- src/solvers/ga/ga_solver_cuda_impl.hpp
+++ src/solvers/ga/ga_solver_cuda_impl.hpp
@@ -2,124 +2,131 @@
 
 namespace locusta {
 
-  template<typename TFloat>
-  void elite_population_replace_dispatch(const uint32_t ISLES,
-                                         const uint32_t AGENTS,
-                                         const uint32_t DIMENSIONS,
-                                         const uint32_t * min_agent_idx,
-                                         const TFloat * max_agent_genome,
-                                         const TFloat * max_agent_fitness,
-                                         TFloat * genomes,
-                                         TFloat * fitness);
+template <typename TFloat>
+void elite_population_replace_dispatch(
+  const uint32_t ISLES, const uint32_t AGENTS, const uint32_t DIMENSIONS,
+  const uint32_t* min_agent_idx, const TFloat* max_agent_genome,
+  const TFloat* max_agent_fitness, TFloat* genomes, TFloat* fitness);
 
-  ///Interface for Genetic Algorithm solvers
-  template<typename TFloat>
-  ga_solver_cuda<TFloat>::ga_solver_cuda(population_set_cuda<TFloat> * population,
-                                         evaluator_cuda<TFloat> * evaluator,
-                                         prngenerator_cuda<TFloat> * prn_generator,
-                                         uint32_t generation_target,
-                                         TFloat * upper_bounds,
-                                         TFloat * lower_bounds)
+/// Interface for Genetic Algorithm solvers
+template <typename TFloat>
+ga_solver_cuda<TFloat>::ga_solver_cuda(population_set_cuda<TFloat>* population,
+                                       evaluator_cuda<TFloat>* evaluator,
+                                       prngenerator_cuda<TFloat>* prn_generator,
+                                       uint32_t generation_target,
+                                       TFloat* upper_bounds,
+                                       TFloat* lower_bounds)
 
-    : evolutionary_solver_cuda<TFloat>(population,
-                                       evaluator,
-                                       prn_generator,
-                                       generation_target,
-                                       upper_bounds,
-                                       lower_bounds) {
-    // Defaults
-    _migration_step = 0;
-    _migration_size = 1;
-    _migration_selection_size = 2;
-    _selection_size = 2;
-    _selection_stochastic_factor = 0;
-    _crossover_rate = 0.9;
-    _mutation_rate = 0.1;
+  : evolutionary_solver_cuda<TFloat>(population, evaluator, prn_generator,
+                                     generation_target, upper_bounds,
+                                     lower_bounds)
+{
+  // Defaults
+  _migration_step = 0;
+  _migration_size = 1;
+  _migration_selection_size = 2;
+  _selection_size = 2;
+  _selection_stochastic_factor = 0;
+  _crossover_rate = 0.9;
+  _mutation_rate = 0.1;
 
-    // Allocate GA resources
-    const size_t TOTAL_AGENTS = _population->_TOTAL_AGENTS;
+  // Allocate GA resources
+  const size_t TOTAL_AGENTS = _population->_TOTAL_AGENTS;
 
-    CudaSafeCall(cudaMalloc((void **) &(_dev_couples_idx_array), TOTAL_AGENTS * sizeof(uint32_t)));
-    CudaSafeCall(cudaMalloc((void **) &(_dev_candidates_reservoir_array), _ISLES * _AGENTS * _AGENTS * sizeof(uint32_t)));
-  }
+  CudaSafeCall(cudaMalloc((void**)&(_dev_couples_idx_array),
+                          TOTAL_AGENTS * sizeof(uint32_t)));
+  CudaSafeCall(cudaMalloc((void**)&(_dev_candidates_reservoir_array),
+                          _ISLES * _AGENTS * _AGENTS * sizeof(uint32_t)));
+}
 
-  template<typename TFloat>
-  ga_solver_cuda<TFloat>::~ga_solver_cuda() {
-    CudaSafeCall(cudaFree(_dev_couples_idx_array));
-    CudaSafeCall(cudaFree(_dev_candidates_reservoir_array));
-  }
+template <typename TFloat>
+ga_solver_cuda<TFloat>::~ga_solver_cuda()
+{
+  CudaSafeCall(cudaFree(_dev_couples_idx_array));
+  CudaSafeCall(cudaFree(_dev_candidates_reservoir_array));
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::setup_solver() {
-    // Pseudo random number allocation.
-    const uint32_t SELECTION_OFFSET = _selection_functor_ptr->required_prns(this);
-    const uint32_t BREEDING_OFFSET = _breed_functor_ptr->required_prns(this);
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::setup_solver()
+{
+  // Pseudo random number allocation.
+  const uint32_t SELECTION_OFFSET = _selection_functor_ptr->required_prns(this);
+  const uint32_t BREEDING_OFFSET = _breed_functor_ptr->required_prns(this);
 
-    _bulk_size = SELECTION_OFFSET + BREEDING_OFFSET;
-    CudaSafeCall(cudaMalloc((void **) &(_dev_bulk_prns), _bulk_size * sizeof(TFloat)));
+  _bulk_size = SELECTION_OFFSET + BREEDING_OFFSET;
+  CudaSafeCall(
+    cudaMalloc((void**)&(_dev_bulk_prns), _bulk_size * sizeof(TFloat)));
 
-    _prn_sets = new TFloat*[2];
-    _prn_sets[SELECTION_SET] = _dev_bulk_prns;
-    _prn_sets[BREEDING_SET] = _dev_bulk_prns + SELECTION_OFFSET;
+  _prn_sets = new TFloat*[2];
+  _prn_sets[SELECTION_SET] = _dev_bulk_prns;
+  _prn_sets[BREEDING_SET] = _dev_bulk_prns + SELECTION_OFFSET;
 
-    evolutionary_solver_cuda<TFloat>::setup_solver();
-  }
+  evolutionary_solver_cuda<TFloat>::setup_solver();
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::teardown_solver() {
-    delete [] _prn_sets;
-    CudaSafeCall(cudaFree(_dev_bulk_prns));
-  }
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::teardown_solver()
+{
+  delete[] _prn_sets;
+  CudaSafeCall(cudaFree(_dev_bulk_prns));
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::setup_operators(BreedCudaFunctor<TFloat> * breed_functor_ptr,
-                                               SelectionCudaFunctor<TFloat> * selection_functor_ptr) {
-    _breed_functor_ptr = breed_functor_ptr;
-    _selection_functor_ptr = selection_functor_ptr;
-  }
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::setup_operators(
+  BreedCudaFunctor<TFloat>* breed_functor_ptr,
+  SelectionCudaFunctor<TFloat>* selection_functor_ptr)
+{
+  _breed_functor_ptr = breed_functor_ptr;
+  _selection_functor_ptr = selection_functor_ptr;
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::solver_config(uint32_t migration_step,
-                                             uint32_t migration_size,
-                                             uint32_t migration_selection_size,
-                                             uint32_t selection_size,
-                                             TFloat selection_stochastic_factor,
-                                             TFloat crossover_rate,
-                                             TFloat mutation_rate) {
-    _migration_step = migration_step;
-    _migration_size = migration_size;
-    _migration_selection_size = migration_selection_size;
-    _selection_size = selection_size;
-    _selection_stochastic_factor = selection_stochastic_factor;
-    _crossover_rate = crossover_rate;
-    _mutation_rate = mutation_rate;
-  }
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::solver_config(uint32_t migration_step,
+                                      uint32_t migration_size,
+                                      uint32_t migration_selection_size,
+                                      uint32_t selection_size,
+                                      TFloat selection_stochastic_factor,
+                                      TFloat crossover_rate,
+                                      TFloat mutation_rate)
+{
+  _migration_step = migration_step;
+  _migration_size = migration_size;
+  _migration_selection_size = migration_selection_size;
+  _selection_size = selection_size;
+  _selection_stochastic_factor = selection_stochastic_factor;
+  _crossover_rate = crossover_rate;
+  _mutation_rate = mutation_rate;
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::transform() {
-    elite_population_replace();
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::transform()
+{
+  elite_population_replace();
 
-    (*_selection_functor_ptr)(this);
-    (*_breed_functor_ptr)(this);
+  (*_selection_functor_ptr)(this);
+  (*_breed_functor_ptr)(this);
 
-    // Crop transformation vector
-    evolutionary_solver_cuda<TFloat>::crop_vector(_dev_population->_dev_transformed_data_array);
-  }
+  // Crop transformation vector
+  // evolutionary_solver_cuda<TFloat>::crop_vector(
+  //   _dev_population->_dev_transformed_data_array);
+}
 
-  template<typename TFloat>
-  void ga_solver_cuda<TFloat>::elite_population_replace() {
+template <typename TFloat>
+void
+ga_solver_cuda<TFloat>::elite_population_replace()
+{
 
-    TFloat * genomes = _dev_population->_dev_data_array;
-    TFloat * fitness = _dev_population->_dev_fitness_array;
+  TFloat* genomes = _dev_population->_dev_data_array;
+  TFloat* fitness = _dev_population->_dev_fitness_array;
 
-    elite_population_replace_dispatch(_ISLES,
-                                      _AGENTS,
-                                      _DIMENSIONS,
-                                      _dev_min_agent_idx,
-                                      _dev_max_agent_genome,
-                                      _dev_max_agent_fitness,
-                                      genomes,
-                                      fitness);
-  }
+  elite_population_replace_dispatch(_ISLES, _AGENTS, _DIMENSIONS,
+                                    _dev_min_agent_idx, _dev_max_agent_genome,
+                                    _dev_max_agent_fitness, genomes, fitness);
+}
 
 } // namespace locusta
